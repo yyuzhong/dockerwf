0c02d4bf9940a9e91bd5887c679bf61ce857e8c2
yyuzhong <yyuzhong@hotmail.com>
2015-05-19 16:09:05 -0700

    [YZ] map root dir and pass parameters

--- rscript/test.R
+++ rscript/test.R
@@ -1,5 +1,28 @@
-A <- read.table("x.data", sep=",", col.names=c("year,","my1","my2"))
+library('getopt');
+opt = getopt(matrix(c(
+    'input', 'i',1,"character",
+    'output','o',1,"character",
+    'parameter','p',1,"integer",
+    'help','h',0,"logical"
+),ncol=4,byrow=TRUE));
 
-nrow(A)
-summary(A$year)
+if(!is.null(opt$help)) {
+    self = commandArgs()[1];
+    cat(paste("Usage: ",self, "[-[-input|i] <input file>] [-[-output|o] <output file>] [-[-parameter|p] <parameter>]\n",sep=""));
+
+}
+cat(opt$input);
+cat("\n");
+
+cat(opt$output);
+cat("\n");
+
+cat(opt$parameter);
+cat("\n");
+
+inputfile <- opt$input
+A <- read.table(inputfile, sep=",", col.names=c("year,","my1","my2"));
+
+nrow(A);
+summary(A$year);
 
