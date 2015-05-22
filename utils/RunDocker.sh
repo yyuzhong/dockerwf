ba7e07b32e86e45c6de1c7d04e316fcd9789ad52
yyuzhong <yyuzhong@hotmail.com>
2015-05-22 17:37:30 -0400

    [YZ] change to run in workflow

--- utils/RunDocker.sh
+++ utils/RunDocker.sh
@@ -1,17 +1,18 @@
 #/bin/bash
 
 if [ "$#" -lt 4 ]; then
-    echo "Usage: $0 imagename workdir script argumeents" >&2
+    echo "Usage: $0 imagename workdir script arguments" >&2
     exit 1
 fi
 
-myimage=$1; # yyuzhong/myrimage:latest
+myimage=$1; # yyuzhong/xxxx:latest
 mydir=$2;   # /home/yzyan/dockerwf
 myapp=$3;   # test.R
 myargs=${@:4};    # -i rdata/x.data -o output.data -p 100
 workspace="/tmp/rstage/"
+scriptdir="/home/workspace/rscript/"
 
-subname=`echo $myimage| cut -d':' -f 1` #find the main name yyuzhong/myrimage
+subname=`echo $myimage| cut -d':' -f 1` #find the main name yyuzhong/xxxx
 
 if docker images | grep "$subname" > /dev/null
 then
@@ -25,7 +26,7 @@ WORKDIR=$( cd $(dirname $0) ; pwd -P)
 docker ps -a | grep myrwidget | awk '{print $1}' | xargs docker stop
 docker ps -a | grep myrwidget | awk '{print $1}' | xargs docker rm
 
-myscript="$workspace$myapp"
+myscript="$scriptdir$myapp"
 
 docker run --name myrwidget -v $mydir:$workspace -w $workspace --privileged=true "$myimage" Rscript $myscript $myargs
 
